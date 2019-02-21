2cb981cd0e045db829ebaaabf84b7e5158c5c0bb
MarkyMark11 <47856837+MarkyMark11@users.noreply.github.com>
2019-02-21 14:18:11 +0000

    Add files via upload

---
+++ Basic_Pot_example.ino
@@ -0,0 +1,12 @@
+#include <potentiometer.h>
+potWmap an_inp;                     //Class.Object
+
+void setup() {
+  Serial.begin(9600);               //Baud Rate
+  an_inp.setup_pot(A0,-1,1);        //(Input Pin, Lower Parameter, Upper Parameter)
+}
+
+void loop() {
+  float val = an_inp.map_input();  //Call function from class and assign to float
+  Serial.pintln(val);              //Print Val to new line
+}
