b7cd4f3c3887321efd5b2b5585da33bc0172a185
MarkyMark11 <47856837+MarkyMark11@users.noreply.github.com>
2019-02-27 13:39:01 +0000

    Update potentiometer.h
    Will return PWM value from between 100 and 255. Minimum value of 100 prevents motor from stalling.

--- potentiometer.h
+++ potentiometer.h
@@ -21,8 +21,8 @@ class potWmap{                                                      	//Create Cl
 		float map_input()
 		{
 			float B_val=analogRead(AnalogPin);							//Floating Point Integerto store value read from input pin
-			const int min_beta_val = -1;								//Constant Integer to store Minimum value = -1
-			float val= min_beta_val + B_val/(1023/2);					//Equation to return result between 1 and -1 stored as floating point variable
+			const int min_beta_val = 100;								//Constant Integer to store Minimum value = -1
+			float val= min_beta_val + B_val/(1023/155);					//Equation to return result between 1 and -1 stored as floating point variable
 			B_val=val;                                                  //Overwirte B_val with val
 			return val;                                                 //Return value to allow acess by other functons/classes
 		}
