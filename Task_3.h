7e8acd4376ea75d451831bc5c046d0270c7b2ce4
MarkyMark11 <47856837+MarkyMark11@users.noreply.github.com>
2019-03-03 18:16:17 +0000

    Add files via upload

--- Task_3.h
+++ Task_3.h
@@ -2,97 +2,104 @@
 #define Task_3_H	
 #include "Check_timer.h"
 
-	class Task_3{
-	protected:
+
+class Task_3 {
+protected:
 	PID_System PID;
 	OpenLoopSystem OLS;
 	IntervalCheckTimer Time;
+	InterruptSpeedMeasure_SMA interrupt;
+
+public:
+	Task_3()
+	{interrupt.setupSpeedMeasure(int_0);}								//Constructor for Hall effect sensor
 	
-	public:
-		double rise_time;															//Global Variable for rist time
-		double peak_time; 															//Global Variable for peak time
-		double min_time; 															//Global Variable for peak time
-		double settling_time;
-		int peak_val;																//Global Variable for peak value
-		int min_val;																//Global Variable for peak value
-		bool peak_val_flag;
-		bool min_val_flag;
-		
-		double rise_time_fun()														//Function To Return Value at 90% of target
-		{
-			unsigned long timer = millis();											//Start timer
-			int Current_output = PID.getRPM();									//Obtain Current PWM output
-			int Target_Speed = PID.getTargetSpeed();								//Obtain Target speed
-			if (PID.PID_PWM && (current_output = Target_Speed * 0.9))				//If pid-pwm is called and 90% target...
-			double rise_time = timer; 												//Save time
-			return rise_time;														//Return to allow external access
-		}
+	double rise_time;													//Global Variable for rist time
+	double peak_time; 													//Global Variable for peak time
+	double min_time; 													//Global Variable for minimum settling time
+	double settling_time;												//Global Variable for settling time
+	int peak_val;														//Global Variable for peak value
+	int min_val;														//Global Variable for minimum settling value
+	bool peak_val_flag;													//Peak Value Flag
+	bool min_val_flag;													//Minimum Value Flag
 
-		double peak_vals_fun()														//Function to record peak value, and time of incident
+
+	double rise_time_fun(int Target_Speed, int current_output)			
+	{
+		double g_rise_time;												//Initialise Local Variable to store Rise Time
+		long timer = millis();											//Start timer
+		if ((double)current_output >= (double)Target_Speed * 0.9)		//If current speed = 90% of target...
 		{
-			int current_RPM;
-			Time.IntervalCheckTimer(50);
-			unsigned long timer = millis();											//Start timer					
-			int RPM = OLS.getRPM();													//Obtain Current RPM
-			int prev_RPM = RPM;														//Record RPM
-			if(Time.isMinChekTimeElapsed()==true)
-			{
-				current_RPM = OLS.getRPM();	
-			}										
-			if (PID.PID_PWM && current_RPM > prev_RPM)								//If measured value is greater than previous value...
-			peak_val = currentRPM;													//Record Speed
-			peak_time = timer;														//Record Time
-			peak_val_flag = true;													//Peak Value Flag is tripped
-			return peak_val;														//Return to allow external access										
+			double rise_time = timer;
+			g_rise_time = rise_time;
 		}
-		
-		double min_vals_fun()
+		return g_rise_time;												//Return to allow external access
+	}
+
+	double peak_vals_fun(int Target_Speed, int& current_output)			//Function to record peak value, and time of incident
+	{
+		int current_RPM;
+		Time.setInterCheck(50);											//Set Interval Check Time to 50ms
+		long timer = millis();											//Start timer					
+		int prev_RPM = current_output;									//Record RPM
+		if (Time.isMinChekTimeElapsed() == true)						//If timeer check has elapsed
 		{
-			int current_RPM;
-			Time.IntervalCheckTimer(50);
-			if(peak_val_flag)
-			unsigned long timer = millis();										//Start timer					
-			int RPM = OLS.getRPM();												//Obtain Current RPM
-			int prev_RPM = RPM;													//Record RPM
-			if(Time.isMinChekTimeElapsed()==true)
-			{
-				current_RPM = OLS.getRPM();	
-			}									//Do it again									
-			if (current_RPM < prev_RPM)											//If measured value is less than previous value...
-			min_val = currentRPM;												//Record Value
-			min_time = timer;													//Record Time
-			return min_val;														//Return to allow external access		
+			current_RPM = current_output;								//Save current RPM
 		}
-		
-		double settling_time_fun()
+		if (current_RPM >= prev_RPM)									//If measured value is greater than previous value...
 		{
-			unsigned long timer = millis();										//Start timer	
-			int Current_RPM = OLS.getRPM();										//Obtain Current RPM
-			int Target_RPM = PID.getTargetSpeed();								//Obtain Target RPM
+			peak_val = current_RPM;										//Record Speed
+		}
+		peak_time = timer;												//Record Time
+		peak_val_flag = true;											//Peak Value Flag set to true
+		return peak_val;												//Return to allow external access
+	}
+
+	double min_vals_fun(int Target_Speed, int current_output)
+	{
+		int current_RPM;												//Variable to store current RPM
+		long timer;														//Blank timer variable
+		Time.setInterCheck(50);											//Set Interval Check Time to 50ms
+		if (peak_val_flag)												//If peak value flag is true...
+		timer = millis();												//Start timer					
+		int RPM = current_output;										//Obtain Current RPM
+		int prev_RPM = RPM;												//Obtain Current RPM
+		if (Time.isMinChekTimeElapsed() == true)						//If check time has elapsed
+		{
+			current_RPM = current_output;								//Overwrite current RPM with measured value
+		}																		
+		if (current_RPM < prev_RPM)										//If measured value is less than previous value...
+		min_val = current_RPM;											//Record Value
+		min_time = timer;												//Record Time of ocurrance
+		return min_val;													//Return to allow external access		
+	}
+
+	double settling_time_fun(int Target_Speed, int current_output)
+	{
+		long timer = millis();											//Start timer	
+		int Current_RPM = current_output;								//Obtain Current RPM
+		int Target_RPM = Target_Speed;									//Obtain Target RPM
 			//If value is within 10% tollerances, and minumum flag is tripped
-			if ((Current_RPM = 1.1*Target_RPM || Current_RPM = 0.9*Target_RPM )&& min_val_flag)
-			return settling_time;												//Return to allow external access
-		}
-		
-		void print_task_3_vals()												//Function to print to serial monitor
-		{
-			rise_time_fun();
-			peak_vals_fun();
-			min_vals_fun();
-			settling_time_fun();
-			Serial.println("Rise Time: ");
-			Serial.print(rise_time);
-			Serial.println("Peak Value: ");
-			Serial.print(peak_val);
-			Serial.println("Time Of Last Peak Value: ");
-			Serial.print(peak_time);
-			Serial.println("Minimum Settling Value: ");
-			Serial.print(min_val);
-			Serial.println("Minimum Settling Time: ");
-			Serial.print(min_time);
-			Serial.println("Settling Time: ");
-			Serial.print(settling__time);			
-		}
-	};
+			if (((double)Current_RPM == 1.1*(double)Target_RPM || (double)Current_RPM == 0.9*(double)Target_RPM) && min_val_flag)
+			return settling_time;										//Return to allow external access
+	}
 
+	void print_task_3_vals(int Target_Speed, int current_output)		
+	{
+		int inp_target_speed = Target_Speed;
+		int inp_current_output = current_output;
+		Serial.println("Rise Time: ");
+		Serial.println(rise_time_fun(inp_target_speed, inp_current_output));
+		Serial.println("Peak Value: ");
+		Serial.println(peak_vals_fun(inp_target_speed, inp_current_output));
+		Serial.println("Time Of Last Peak Value: ");
+		Serial.println(peak_time);
+		Serial.println("Minimum Settling Value: ");
+		Serial.println(min_vals_fun(inp_target_speed, inp_current_output));
+		Serial.println("Minimum Settling Time: ");
+		Serial.println(min_time);
+		Serial.println("Settling Time: ");
+		Serial.println(settling_time_fun(inp_target_speed, inp_current_output));	
+	}
+};
 #endif
